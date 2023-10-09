6d6a715e74604ea9fadf04bada936fdb30795648
mikolai.spocinski <spocinskimike@gmail.com>
2023-10-09 13:52:52 +0100

    implemented and tested up to isInRegion

--- src/main/java/uk/ac/ed/inf/LngLatHandler.java
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -1,8 +1,11 @@
 package uk.ac.ed.inf;
 
+import uk.ac.ed.inf.ilp.constant.SystemConstants;
 import uk.ac.ed.inf.ilp.data.LngLat;
 import uk.ac.ed.inf.ilp.data.NamedRegion;
 import uk.ac.ed.inf.ilp.interfaces.LngLatHandling;
+
+import java.util.*;
 
 public class LngLatHandler implements LngLatHandling {
     public double distanceTo(LngLat startPosition, LngLat endPosition) {
@@ -12,12 +15,16 @@ public class LngLatHandler implements LngLatHandling {
         double x2 = endPosition.lng();
         double y2 = endPosition.lat();
 
+        //Return Euclidean distance
         return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
     }
 
 
     public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
-        return false;
+
+        //If points are <= DRONE_IS_CLOSE_DISTANCE, returns true, otherwise returns false
+        return distanceTo(startPosition, otherPosition) <= SystemConstants.DRONE_IS_CLOSE_DISTANCE;
+
     }
 
     public boolean isInCentralArea(LngLat point, NamedRegion centralArea) {
@@ -25,7 +32,41 @@ public class LngLatHandler implements LngLatHandling {
     }
 
     public boolean isInRegion(LngLat position, NamedRegion region) {
-        return false;
+        int numVertices = region.vertices().length;
+        int intersectionCount = 0;
+
+        for (int i = 0; i < numVertices; i++) {
+            LngLat vertex1 = region.vertices()[i];
+            LngLat vertex2 = region.vertices()[(i + 1) % numVertices];
+
+            if (pointOnSegment(position, vertex1, vertex2)) {
+                return true; // Point is on the edge of the polygon
+            }
+
+            if (position.lat() > Math.min(vertex1.lat(), vertex2.lat()) &&
+                position.lat() <= Math.max(vertex1.lat(), vertex2.lat()) &&
+                position.lng() <= Math.max(vertex1.lng(), vertex2.lng()) &&
+                    vertex1.lat() != vertex2.lat()) {
+                double xIntersection = (position.lat() - vertex1.lat()) * (vertex2.lng() - vertex1.lng()) / (vertex2.lat() - vertex1.lat()) + vertex1.lng();
+
+                if (vertex1.lng() == vertex2.lng() || position.lng() <= xIntersection) {
+                    intersectionCount++;
+                }
+            }
+
+        }
+
+        return intersectionCount % 2 == 1;
+
+    }
+
+    public static boolean pointOnSegment(LngLat position, LngLat start, LngLat end) {
+        return (position.lng() == start.lng() && position.lat() == start.lat()) ||
+                (position.lng() == end.lng() && position.lat() == end.lat()) ||
+                (position.lng() >= Math.min(start.lng(), end.lat()) &&
+                        position.lng() <= Math.max(start.lng(), end.lng()) &&
+                        position.lat() >= Math.min(start.lat(), end.lat()) &&
+                        position.lat() <= Math.max(start.lat(), end.lat()));
     }
 
     public LngLat nextPosition(LngLat startPosition, double angle) {
--- src/main/java/uk/ac/ed/inf/Main.java
+++ src/main/java/uk/ac/ed/inf/Main.java
@@ -1,5 +1,8 @@
 package uk.ac.ed.inf;
 
+import uk.ac.ed.inf.ilp.data.LngLat;
+import uk.ac.ed.inf.ilp.data.NamedRegion;
+
 /**
  * Hello world!
  *
@@ -15,5 +18,29 @@ public class Main
 
         LngLatHandler testLngLatHandler = new LngLatHandler();
         System.out.println("testLngLatHandler Instantiated");
+
+        //test variables
+        LngLat point1 = new LngLat(-5.192473, 60.526233);
+        LngLat point2 = new LngLat(-5.992473, 50.942627);
+        LngLat point3 = new LngLat(5.192473, 50.816933);
+        LngLat point4 = new LngLat(5.992473, 60.146233);
+
+        LngLat pointIn = new LngLat(-4.192473, 55.526233);
+        LngLat pointOut = new LngLat(-6.192473, 65.526233);
+
+        LngLat [] regionVertices = {point1, point2, point3, point4};
+
+        NamedRegion testNamedRegion = new NamedRegion("testRegion", regionVertices);
+
+        //testing the functions
+        double distanceToTest = testLngLatHandler.distanceTo(point1, point3);
+        System.out.println("distanceTo: " + distanceToTest);
+
+        boolean isCloseToTest = testLngLatHandler.isCloseTo(point1, point1);
+        System.out.println("isCloseTo: " + isCloseToTest);
+
+        boolean isInRegionTest = testLngLatHandler.isInRegion(point1, testNamedRegion);
+        System.out.println("isInRegion: " + isInRegionTest);
+
     }
 }
