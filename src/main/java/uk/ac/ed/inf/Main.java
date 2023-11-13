23baf4ba215bb8d37f84dea35e8d56382f932cf4
mikolai.spocinski <spocinskimike@gmail.com>
2023-11-13 15:25:41 +0000

    Implemented rest data retrieval

--- src/main/java/uk/ac/ed/inf/Main.java
+++ src/main/java/uk/ac/ed/inf/Main.java
@@ -1,93 +1,52 @@
 package uk.ac.ed.inf;
 
-import uk.ac.ed.inf.ilp.data.LngLat;
 import uk.ac.ed.inf.ilp.data.NamedRegion;
-import uk.ac.ed.inf.ilp.data.Restaurant;
-import uk.ac.ed.inf.ilp.data.Pizza;
 import uk.ac.ed.inf.ilp.data.Order;
-import uk.ac.ed.inf.ilp.data.CreditCardInformation;
+import uk.ac.ed.inf.ilp.data.Restaurant;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Hello world!
  *
  */
-public class Main
-{
-    public static void main( String[] args )
+public class Main {
+
+
+    public static void main(String[] args){
+
+    /*  ACTUAL CODE
     {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar PizzaDronz-1.0-SNAPSHOT.jar <date> <url>");
+            System.exit(1);
+        }
 
-        // LngLatHandler testing
-        System.out.println( "Hello World!" );
+        String date = args[0];
+        String uri = args[1];
 
-        OrderValidator testOrderValidator = new OrderValidator();
-        System.out.println("testOrderValidator Instantiated");
+     */
 
-        LngLatHandler testLngLatHandler = new LngLatHandler();
-        System.out.println("testLngLatHandler Instantiated");
-
-        //test variables
-        LngLat point1 = new LngLat(-5.192473, 60.526233);
-        LngLat point2 = new LngLat(-5.992473, 50.942627);
-        LngLat point3 = new LngLat(5.192473, 50.816933);
-        LngLat point4 = new LngLat(5.992473, 60.146233);
-
-        LngLat pointIn = new LngLat(-4.192473, 55.526233);
-        LngLat pointOut = new LngLat(-6.192473, 65.526233);
-
-        LngLat easyPoint = new LngLat(10, -10);
-
-        LngLat [] regionVertices = {point1, point2, point3, point4};
-
-        NamedRegion testNamedRegion = new NamedRegion("testRegion", regionVertices);
+    //TESTING TO AVOID COMPILING
+    String date = "2023-09-01";
+    String uri = "https://ilp-rest.azurewebsites.net";
 
 
-        // Order validator testing
-        DayOfWeek [] monToWed = {DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY};
-        DayOfWeek [] weekdays = {DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY};
+    //Initialise REST data retriever
+    RestDataRetriever restDataRetriever = new RestDataRetriever();
 
-        Pizza margherita = new Pizza("Margherita", 600);
-        Pizza parmigiana = new Pizza("Parmigiana", 800);
+    //Retrieve central area
+   NamedRegion centralArea = restDataRetriever.getCentralArea(uri);
 
-        Pizza meaty = new Pizza("Meaty", 700);
-        Pizza veggie = new Pizza("Veggie", 900);
+    //Retrieve list of no-fly zones
+    NamedRegion[] noFlyZones = restDataRetriever.getNoFlyZones(uri);
 
-        Pizza [] pizzaPostoMenu = {margherita, parmigiana};
-        Pizza [] testMenu2 = {meaty, veggie};
+    //Retrieve list of restaurants
+    Restaurant[] restaurants = restDataRetriever.getRestaurants(uri);
 
-        Restaurant testRestaurant1 = new Restaurant("Pizza Posto", pointIn, monToWed, pizzaPostoMenu);
-        Restaurant testRestaurant2 = new Restaurant("Restaurant 2", pointIn, weekdays, testMenu2);
+    //Retrieve list of orders
 
-        Restaurant [] ourRestaurants = {testRestaurant1, testRestaurant2};
-
-        LocalDate today = LocalDate.of(2023, 10, 10);
-
-        CreditCardInformation testCardValid = new CreditCardInformation("4844446683665696", "09/2028", "596");
-
-        Order testOrder1 = new Order("19514FE0", today, 1400, pizzaPostoMenu, testCardValid);
-
-
-
-        //testing the functions
-        double distanceToTest = testLngLatHandler.distanceTo(point1, point3);
-        System.out.println("distanceTo: " + distanceToTest);
-
-        boolean isCloseToTest = testLngLatHandler.isCloseTo(point1, point1);
-        System.out.println("isCloseTo: " + isCloseToTest);
-
-        boolean isInRegionTest = testLngLatHandler.isInRegion(point1, testNamedRegion);
-        System.out.println("isInRegion: " + isInRegionTest);
-
-        LngLat nextPosTest = testLngLatHandler.nextPosition(easyPoint, 45);
-        System.out.println("nextPos: " + nextPosTest);
-
-
-        System.out.println(testOrder1.getOrderValidationCode());
-        testOrder1 = testOrderValidator.validateOrder(testOrder1, ourRestaurants);
-        System.out.println(testOrder1.getOrderValidationCode());
-
+    List<Order> orders = RestDataRetriever.getOrders(uri, date);
 
     }
 }
--- src/main/java/uk/ac/ed/inf/RestDataRetriever.java
+++ src/main/java/uk/ac/ed/inf/RestDataRetriever.java
@@ -1,2 +1,214 @@
-package uk.ac.ed.inf;public class RestDataRetriever {
+package uk.ac.ed.inf;
+import com.google.gson.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import uk.ac.ed.inf.ilp.data.NamedRegion;
+import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.Restaurant;
+import uk.ac.ed.inf.ilp.gsonUtils.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestDataRetriever {
+
+
+    /**
+     *
+     * @param uri the uri of the REST server
+     * @return the CentralArea object containing its name and list of vertices
+     */
+    public NamedRegion getCentralArea(String uri) {
+
+        uri = uri + "/centralArea"; //Set the endpoint for central area
+
+        // Create an HttpClient and HttpGet object
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(uri);
+
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == 200) {
+                //jsonString contains JSON object
+                String jsonString = EntityUtils.toString(response.getEntity());
+
+                //Deserialise JSON String
+                Gson gson = new Gson();
+                return gson.fromJson(jsonString, NamedRegion.class);
+
+
+            } else {
+                System.err.println("HTTP Request failed with status code: " + statusCode);
+                return null;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /**
+     *
+     * @param uri the uri of the REST server
+     * @return array of no-fly zones
+     **/
+    public NamedRegion[] getNoFlyZones(String uri) {
+
+        uri = uri + "/noFlyZones"; //Set the endpoint for central area
+
+        // Create an HttpClient and HttpGet object
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(uri);
+
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == 200) {
+                //jsonString contains JSON object
+                String jsonString = EntityUtils.toString(response.getEntity());
+
+                //Deserialise JSON String
+                Gson gson = new Gson();
+                return gson.fromJson(jsonString, NamedRegion[].class);
+
+
+            } else {
+                System.err.println("HTTP Request failed with status code: " + statusCode);
+                return null;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /**
+     *
+     * @param uri the uri of the REST server
+     * @return array of participating restaurants
+     */
+    public Restaurant[] getRestaurants(String uri) {
+
+        uri = uri + "/restaurants"; //Set the endpoint for central area
+
+        // Create an HttpClient and HttpGet object
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(uri);
+
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == 200) {
+                //jsonString contains JSON object
+                String jsonString = EntityUtils.toString(response.getEntity());
+
+                //Deserialise JSON String
+                Gson gson = new Gson();
+                return gson.fromJson(jsonString, Restaurant[].class);
+
+
+            } else {
+                System.err.println("HTTP Request failed with status code: " + statusCode);
+                return null;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static List<Order> getOrders(String uri, String date) {
+        uri = uri + "/orders"; // Set the endpoint for central area
+
+        // Create a Gson instance with the custom serializer and deserializer
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateDeserializer())
+                .registerTypeAdapter(LocalDate.class, new LocalDateSerializer())
+                .create();
+
+        // Create an HttpClient and HttpGet object
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(uri);
+
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == 200) {
+                // jsonString contains JSON object
+                String jsonString = EntityUtils.toString(response.getEntity());
+
+                // Parse JSON data
+                JsonArray jsonArray = JsonParser.parseString(jsonString).getAsJsonArray();
+
+                // Filter objects with the specified date
+                List<JsonElement> filteredOrders = new ArrayList<>();
+                for (JsonElement element : jsonArray) {
+                    JsonObject orderObject = element.getAsJsonObject();
+
+                    // Check if the key "orderDate" exists and the value is not null
+                    if (orderObject.has("orderDate")) {
+                        try {
+                            JsonElement orderDateElement = orderObject.get("orderDate");
+
+                            // Check if the value is not null
+                            if (!orderDateElement.isJsonNull()) {
+                                String orderDateString = orderDateElement.getAsString();
+
+                                // Parse the date string into LocalDate
+                                LocalDate orderDate = LocalDate.parse(orderDateString, DATE_FORMATTER);
+
+                                // Check if the parsed date matches the desired date
+                                if (orderDate.toString().equals(date)) {
+                                    filteredOrders.add(element);
+                                }
+                            }
+                        } catch (Exception e) {
+                            // Print any exception that occurs during parsing
+                            System.err.println("Error parsing order date: " + e.getMessage());
+                        }
+                    }
+                }
+
+                // Convert the filtered orders to Java objects
+                List<Order> orders = new ArrayList<>();
+                for (JsonElement element : filteredOrders) {
+                    try {
+                        Order order = gson.fromJson(element, Order.class);
+                        orders.add(order);
+                    } catch (Exception e) {
+                        // Print any exception that occurs during object conversion
+                        System.err.println("Error converting to Order object: " + e.getMessage());
+                    }
+                }
+
+                return orders;
+            } else {
+                System.err.println("HTTP Request failed with status code: " + statusCode);
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            HttpClientUtils.closeQuietly(httpClient);
+        }
+    }
 }
